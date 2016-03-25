d4e538dbfb5c5da8487caff1e50cf0c5cc3d46cc
Klaus Alfert <klaus.alfert@gmail.com>
2016-03-25 23:45:03 +0100

    Ranch and sasl

--- mix.exs
+++ mix.exs
@@ -24,7 +24,7 @@ defmodule AmqpOne.Mixfile do
   #
   # Type "mix help compile.app" for more information
   def application do
-    [applications: [:logger, :ranch],
+    [applications: [:logger, :ranch, :sasl],
      mod: {AmqpOne, []}]
   end
   # Specifies which paths to compile per environment
