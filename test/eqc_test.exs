24cef0ef4252a7c1b77196b7776f4601b0bd62f3
Klaus Alfert <klaus.alfert@gmail.com>
2016-05-04 23:01:44 +0200

    Also generate atom values properly. But what is happening to utf8?

--- test/eqc_test.exs
+++ test/eqc_test.exs
@@ -7,13 +7,22 @@ defmodule AmqpOne.Test.Quickcheck do
 
   property "encode and decode simple values" do
     list_type = TypeManager.type_spec "list"
-    forall l <- list(simple_amqp_values) do
+    forall l <- non_empty(list(simple_amqp_values)) do
       {decoded, <<>>} = Encoding.typed_encoder(l, list_type) |> Encoding.decode_bin
-      ensure l == decoded
+      collect l: l, in:
+        ensure l == decoded
     end
   end
 
+  @doc "Generator for simple, i.e. unstructed AMQP values"
   def simple_amqp_values do
-    oneof [nat, largeint, bool, utf8, real]
+    oneof [nat, largeint, bool, real, atom] # utf8
+  end
+
+  @doc "Generator for a set of atoms"
+  def atom do
+    let s <- list(char) do
+      :erlang.list_to_atom(s)
+    end
   end
 end
