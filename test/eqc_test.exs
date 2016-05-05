daa5a891f15cc14f979709ad97c9b434a5e57b30
Klaus Alfert <klaus.alfert@gmail.com>
2016-05-05 12:22:16 +0200

    Encoding small arrays

--- test/eqc_test.exs
+++ test/eqc_test.exs
@@ -7,10 +7,21 @@ defmodule AmqpOne.Test.Quickcheck do
 
   property "encode and decode simple values" do
     list_type = TypeManager.type_spec "list"
-    forall l <- non_empty(list(simple_amqp_values)) do
+    forall l <- list(simple_amqp_values) do
       {decoded, <<>>} = Encoding.typed_encoder(l, list_type) |> Encoding.decode_bin
       collect l: l, in:
         ensure l == decoded
+    end
+  end
+
+  property "encode and decode arrays" do
+    array_type = TypeManager.type_spec "array"
+    forall t <- simple_amqp_values do
+      forall a <- vector(10, t) do
+        {decoded, <<>>} = Encoding.typed_encoder(a, array_type) |> Encoding.decode_bin
+        collect a: a, in:
+          ensure a == decoded
+      end
     end
   end
 
